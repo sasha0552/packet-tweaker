1907a4e38eb3daa484c929a9ee4f5b8375552067
sasha0552 <admin@sasha0552.org>
2024-05-03 21:07:20 +0000

    Adapt mod to new build system & api

---
+++ fabric/src/main/java/org/sasha0552/packet_tweaker/fabric/PacketTweakerFabric.java
@@ -0,0 +1,9 @@
+package org.sasha0552.packet_tweaker.fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PacketTweakerFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ forge/src/main/java/org/sasha0552/packet_tweaker/forge/PacketTweakerForge.java
@@ -0,0 +1,9 @@
+package org.sasha0552.packet_tweaker.forge;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("packet_tweaker")
+public class PacketTweakerForge {
+    public PacketTweakerForge() {
+    }
+}
